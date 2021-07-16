28479ee4d08ba2746a35261014ec9db1df0d73b1
steftes99 <stefanotescione83@gmail.com>
2021-07-16 12:35:38 +0200

    init

---
+++ charity/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <header>
+
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+     header{
+        background-color: red;
+        height: 100vh;
+        margin: 0;
+        padding: 0;
+    }
+</style>
---
+++ charity/src/components/Main.vue
@@ -0,0 +1,19 @@
+<template>
+    <main>
+
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style lang="scss" scoped>
+    main{
+        
+        height: 500vh;
+        background-color: yellow;
+    }
+</style>
